86e5a767009565da5148cfbdbe699f771dcca165
Pragati Gautam <pg1407269@gmail.com>
2026-07-06 19:47:39 +0530

    Time: 0 ms (100.00%) | Memory: 30.2 MB (87.60%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,59 +1,25 @@
 class Solution {
 public:
-    vector<int> sortedSquares(vector<int>& nums) { 
-        int size = nums.size();
-        vector<int> neg;
-        vector<int>pos;
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+         vector<int> ans(n);
 
-        for(int i = 0 ; i<size ; i++){
-            if(nums[i] <0)
-            neg.push_back(nums[i]);
-            else
-            pos.push_back(nums[i]);
-        }
-        if(neg.size() == 0){
-            for(int i= 0; i< pos.size() ; i++)
-                pos[i] = pos[i]*pos[i];
-                return pos;
-            
-        }
-        if(pos.size() == 0){
-            for(int i = 0; i< neg.size(); i++)
-            neg[i] = neg[i] * neg[i];
-            reverse(neg.begin(), neg.end());
-            return neg;
-        }
+         int left = 0;
+         int right = n-1 ;
+         int index = n-1;
 
-        int i = 0, j= 0;
-        int id = 0;
-        int m = neg.size();
-        int n = pos.size();
-        vector<int> res(m+n);
-        for(int i= 0; i< m; i++)
-        neg[i] = neg[i] *neg[i];
-        reverse(neg.begin(), neg.end());
-
-        for(int i = 0; i< n; i++)
-        pos[i] =pos[i] * pos[i];
-
-   while(i < m && j < n){
-    if(neg[i] <= pos[j]){
-        res[id++] = neg[i++];
+         while( left <= right){
+            if(abs(nums[left]) > abs(nums[right])){
+            ans[index]= nums[left] * nums[left];
+            left++ ;
+         }
+         else{
+            ans[index] = nums[right] * nums[right];
+            right--;
+         }
+         index--;
+        
     }
-    else{
-        res[id++] = pos[j++];
-    }
-}
-
-while(i < m){
-    res[id++] = neg[i++];
-}
-
-while(j < n){
-    res[id++] = pos[j++];
-}
-      return res;
-       
+    return ans;
     }
 };
-    
